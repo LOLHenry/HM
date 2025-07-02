fd78f53f8a0064901ab9842bae5948c18ea121f9
Remy Foray <remy.foray@allegrodvt.com>
2025-07-02 08:21:33 +0200

    Fix pictures decoding when poc is decreased on an IRAP (pictures were erroneously skipped)
    When an IRAP is not a CRA with NoRaslOutputFlag = 0, the PicOrderCntVal of this
    pictures could be below the previous output pictures, and still need to
    be decoded and output.
    
    m_prevPOC need to be reset in this cases.

--- source/Lib/TLibDecoder/TDecTop.h
+++ source/Lib/TLibDecoder/TDecTop.h
@@ -111,6 +111,7 @@ private:
   TComPic*                m_pcPic;
   UInt                    m_uiSliceIdx;
   Int                     m_prevPOC;
+  NalUnitType             m_prevNut;
   Int                     m_prevTid0POC;
   Bool                    m_bFirstSliceInPicture;
 #if JVET_X0048_X0103_FILM_GRAIN
@@ -125,6 +126,7 @@ private:
   Int                     m_lastPOCNoOutputPriorPics;
   Bool                    m_isNoOutputPriorPics;
   Bool                    m_craNoRaslOutputFlag;    //value of variable NoRaslOutputFlag of the last CRA pic
+  Bool                    m_noRaslOutputFlag;
 #if SHUTTER_INTERVAL_SEI_PROCESSING
   Bool                    m_ShutterFilterEnable;    // Shutter Interval SEI Processing
 #endif
